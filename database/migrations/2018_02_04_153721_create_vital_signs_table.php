7bc8cd4961784daf5546b5e4d0e31b0048c2f7c1
Syed Saad <hassan.saad.mail@gmail.com>
2018-02-04 22:30:35 +0530

    VitalSign Migration Created

---
+++ database/migrations/2018_02_04_153721_create_vital_signs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVitalSignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vital_signs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('desc')->nullable();
+            $table->text('unit')->nullable();
+            $table->string('column_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vital_signs');
+    }
+}
