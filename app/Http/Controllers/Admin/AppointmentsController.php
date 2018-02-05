0d51a205d10c4fc54962d4d6dd0eda12952efb97
Syed Saad <hassan.saad.mail@gmail.com>
2018-02-05 23:14:28 +0530

    Appointment Controller get appointments with date time

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -11,16 +11,35 @@ use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
-
+use Carbon\Carbon;
 class AppointmentsController extends Controller
 {
     public function index()
     {
         $response = new Response();
-        $data = Appointments::all();
+        $data = Appointments::with('patient')->get();
         return $response->getSuccessResponse('Success!', $data);
     }
+    public function get(Request $request)
+    {
+        $response = new Response();
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|date_format:"d-m-Y"',
+            'to' => 'required|date_format:"d-m-Y"|after:from'
+        ]);
+        if ($validator->fails()) {
+            return $response->getValidationError($validator->messages());
+        }
+        $from = $request->input('from');
+        $to = $request->input('to');
+        $to = Carbon::parse($to)->addDay()->format('Y-m-d 23:59:59');
+        $data = Appointments::with('patient')->where([
+            ['scheduled_from', '>=', Carbon::parse($from)->addDay(-1)],
+            ['scheduled_to', '<=', $to],
+        ])->get();
+        return $response->getSuccessResponse("Success!", $data);
 
+    }
     public function store(Request $request) {
         $response = new Response();
 
@@ -70,8 +89,8 @@ class AppointmentsController extends Controller
         $notes = $request->input('notes');
         $doctorId = $request->input('doctor');
         $departmentId = $request->input('department');
-        $doctor = User::find($request->input('doctor'));
-        $department = Departments::find($request->input('department'));
+        $doctor = User::find($doctorId);
+        $department = Departments::find($departmentId)->first();
 
         if($doctorId && !$doctor) {
             return $response->getNotFound('Doctor Not Found');
@@ -84,9 +103,28 @@ class AppointmentsController extends Controller
         $appointment->scheduled_from = $scheduledFrom;
         $appointment->scheduled_to = $scheduledTo;
         $appointment->for_department = $department->id;
-        $appointment->for_doctor = $doctor->id;
+        $appointment->for_doctor = $doctor ? $doctor->id : null;
         $appointment->notes = $notes;
         $appointment->cancelled = false;
+        if(Appointments::where([
+            ['scheduled_from', '<', $scheduledTo],
+            ['scheduled_from', '>', $scheduledFrom],
+        ])
+            ->orWhere([
+                ['scheduled_from', '=', $scheduledFrom],
+                ['scheduled_to', '=', $scheduledTo]
+            ])
+            ->orWhere([
+                ['scheduled_to', '<', $scheduledTo],
+                ['scheduled_to', '>', $scheduledFrom],
+            ])
+            ->orWhere([
+                ['scheduled_from', '<', $scheduledFrom],
+                ['scheduled_to', '>', $scheduledFrom],
+            ])
+            ->first()) {
+            return $response->getAlreadyPresent('Schedule is Already Taken!');
+        }
         try {
             $appointment->save();
         } catch (QueryException $e) {
@@ -119,8 +157,8 @@ class AppointmentsController extends Controller
         $doctorId = $request->input('doctor');
         $departmentId = $request->input('department');
 
-        $doctor = User::find($request->input('doctor'));
-        $department = Departments::find($request->input('department'));
+        $doctor = User::find($doctorId);
+        $department = Departments::find($departmentId);
 
         if($doctorId && !$doctor) {
             return $response->getNotFound('Doctor Not Found');
@@ -128,13 +166,34 @@ class AppointmentsController extends Controller
         if($departmentId && !$department) {
             return $response->getNotFound('Department Not Found');
         }
+            $searchAppointment = Appointments::where([
+                ['scheduled_from', '<', $scheduledTo],
+                ['scheduled_from', '>', $scheduledFrom],
+            ])
+                ->orWhere([
+                    ['scheduled_from', '=', $scheduledFrom],
+                    ['scheduled_to', '=', $scheduledTo]
+                ])
+                ->orWhere([
+                    ['scheduled_to', '<', $scheduledTo],
+                    ['scheduled_to', '>', $scheduledFrom],
+                ])
+                ->orWhere([
+                    ['scheduled_from', '<', $scheduledFrom],
+                    ['scheduled_to', '>', $scheduledFrom],
+                ])
+                ->first();
+            if($searchAppointment && $searchAppointment->id != $appointment->id) {
+                return $response->getAlreadyPresent('Schedule is Already Taken!');
+            }
 
         $appointment->scheduled_from = $scheduledFrom;
         $appointment->scheduled_to = $scheduledTo;
         $appointment->for_department = $department->id;
-        $appointment->for_doctor = $doctor->id;
+        $appointment->for_doctor = $doctor ? $doctor->id : null;
         $appointment->notes = $notes;
-
+        $appointment->cancelled = 0;
+        $appointment->cancel_reason = null;
         try {
             $appointment->save();
         } catch (QueryException $e) {
@@ -146,14 +205,14 @@ class AppointmentsController extends Controller
         $response = new Response();
         $validator = Validator::make($request->all(), [
             'id' => 'required',
-            'deletePermanent' => 'present|boolean',
+            'delete' => 'present|boolean',
             'reason' => 'present'
         ]);
         if ($validator->fails()) {
             return $response->getValidationError($validator->messages());
         }
         $appointment = Appointments::find( $request->input('id'));
-        $deletePermanent = $request->input('deletePermanent');
+        $deletePermanent = $request->input('delete');
         $reason = $request->input('reason');
 
         if(!$appointment) {
